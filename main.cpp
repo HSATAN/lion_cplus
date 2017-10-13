b0f6576fd5886b8ba3a538c6def34353091bdd4c
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-10-13 19:01:28 +0800

    添加merge排序

--- main.cpp
+++ main.cpp
@@ -5,11 +5,53 @@
 #include "tree.h"
 #include <iomanip>
 #include "common.h"
+#include <fstream>
+#include <ctime>
 using namespace std;
 
 
+void new_mergearray(int a[],int first,int mid, int last, int temp[] )
+{
+    int k=0;
+    int i=first;
+    int j=mid+1;
+    int m=mid;
+    int n=last;
+    while (i<=mid && j<=last)
+    {
+        if(a[i]<a[j])
+        {
+            temp[k++]=a[i++];
+        }
+        else
+        {
+            temp[k++]=a[j++];
+        }
 
-
+    }
+    while (i<=m)
+    {
+        temp[k++]=a[i++];
+    }
+    while (j<=last)
+    {
+        temp[k++]=a[j++];
+    }
+    for(i=0;i<k;i++)
+    {
+        a[first+i]=temp[i];
+    }
+}
+void new_mergesort(int a[], int first,int last,int temp[])
+{
+    if(first<last)
+    {
+        int mid=(first+last)/2;
+        new_mergesort(a,first,mid,temp);
+        new_mergesort(a,mid+1,last,temp);
+        new_mergearray(a,first,mid,last,temp);
+    }
+}
 int main() {
     BT *tree;
 //    tree = init(tree,-1);
@@ -33,19 +75,32 @@ int main() {
 //    print_tree_first_format(binarySearchTree.root,0);
 //    cout<<"树是空："<<binarySearchTree.isEmpty()<<endl;
 //    test_map();
-    int a[]={0,5,4,7,3,9,8,1,2,6};
+    int a[]={0,5,4,7,3,9,8,1,2,6,9,10,13};
 //    for(int i=(sizeof(a)/ sizeof(int)-1)/2;i>0;i--)
 //    {
 //        heap_adjust(a,i,sizeof(a)/ sizeof(int)-1);
 //    }
 //    heap_sort(a,sizeof(a)/ sizeof(int)-1);
 //
-    int *p=new int[10];
-    mergesort(a,0,9,p);
-    for (int i=0;i<10;i++)
+    int *p=new int[12];
+    //mergesort(a,0,12,p);
+    new_mergesort(a,0,12,p);
+    for (int i=0;i<13;i++)
     {
         cout<<a[i]<<endl;
     }
+    ifstream in;
+    in.open("/Users/huangkaijie/log/log20171010");
+    int i=0;
+    string s;
+    time_t start = time(NULL);
 
+    while (getline(in,s))
+    {
+        i++;
+    }
+    cout<<i<<endl;
+    time_t end=time(NULL);
+    cout<<end-start<<endl;
     return 0;
 }
